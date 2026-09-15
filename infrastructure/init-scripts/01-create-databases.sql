e83daca9ba19f86177c025023d837b2252353054
sethdevkh <sethdevkh@gmail.com>
2026-09-15 14:22:08 +0700

    Implement persistence rest api adaptor

---
+++ infrastructure/init-scripts/01-create-databases.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE db_order OWNER ecom_user;
+CREATE DATABASE db_payment OWNER ecom_user;
+CREATE DATABASE db_business OWNER ecom_user;
+CREATE DATABASE db_customer OWNER ecom_user;
--- order-service/order-service-main/src/main/java/com/sethdevkh/ecommerce/system/order/service/OrderServiceApplication.java
+++ order-service/order-service-main/src/main/java/com/sethdevkh/ecommerce/system/order/service/OrderServiceApplication.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
 @EntityScan(basePackages = {
-        "com.sethdevkh.ecommerce.system.order.service.persistence"
+        "com.sethdevkh.ecommerce.system.order.service.persistence.entity"
 })
 @EnableJpaRepositories(basePackages = {
-        "com.sethdevkh.ecommerce.system.order.service.persistence"
+        "com.sethdevkh.ecommerce.system.order.service.persistence.repository"
 })
+@SpringBootApplication
 public class OrderServiceApplication {
     static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
