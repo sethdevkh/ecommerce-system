181b79488ad98bca368134ec4625c4cf7696b7bf
sethdevkh <sethdevkh@gmail.com>
2026-09-26 21:07:56 +0700

    clean common-domain

--- common/common-domain/src/main/java/com/sethdevkh/ecommerce/system/domain/valueobject/Money.java
+++ common/common-domain/src/main/java/com/sethdevkh/ecommerce/system/domain/valueobject/Money.java
@@ -7,49 +7,48 @@ import java.util.Objects;
 public class Money {
     private final BigDecimal amount;
 
-    public static final Money ZERO = new Money(BigDecimal.ZERO);
-
     public Money(BigDecimal amount) {
         this.amount = amount;
-    }
-
-    public boolean isGreaterThanZero() {
-        return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
-    }
-
-    public boolean isGreaterThan(Money money) {
-        return this.amount != null && this.amount.compareTo(money.getAmount()) > 0;
-    }
-
-    public Money add(Money money) {
-        return new Money(setScale(this.amount.add(money.getAmount())));
-    }
-
-    public Money subtract(Money money) {
-        return new Money(setScale(this.amount.subtract(money.getAmount())));
-    }
-
-    public Money multiply(int multiplier) {
-        return new Money(setScale(this.amount.multiply(new BigDecimal(multiplier))));
     }
 
     public BigDecimal getAmount() {
         return amount;
     }
 
+    public static final Money ZERO = new Money(BigDecimal.ZERO);
+
+    public boolean isGreaterThanZero() {
+        return amount.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean isGreaterThan(Money money) {
+        return amount.compareTo(money.amount) > 0;
+    }
+
+    public Money add(Money money) {
+        return new Money(setScale(this.amount.add(money.amount)));
+    }
+
+    public Money subtract(Money money) {
+        return new Money(setScale(this.amount.subtract(money.amount)));
+    }
+
+    public Money multiply(int multiplier) {
+        return new Money(setScale(this.amount.multiply(BigDecimal.valueOf(multiplier))));
+    }
+
+    private BigDecimal setScale(BigDecimal inputAmount) {
+        return inputAmount.setScale(2, RoundingMode.HALF_EVEN);
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Money money = (Money) o;
-        return Objects.equals(amount, money.amount);
+        if (!(o instanceof Money money)) return false;
+        return Objects.equals(setScale(amount), setScale(money.amount));
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(amount);
-    }
-
-    private BigDecimal setScale(BigDecimal input) {
-        return input.setScale(2, RoundingMode.HALF_EVEN);
+        return Objects.hashCode(setScale(amount));
     }
 }
