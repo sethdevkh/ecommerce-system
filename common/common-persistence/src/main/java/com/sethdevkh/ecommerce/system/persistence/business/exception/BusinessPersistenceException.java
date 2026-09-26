8f30174e7eff1f80d990c06bd8f12cc3bae58530
sethdevkh <sethdevkh@gmail.com>
2026-09-26 21:10:30 +0700

    clean common-persistence

---
+++ common/common-persistence/src/main/java/com/sethdevkh/ecommerce/system/persistence/business/exception/BusinessPersistenceException.java
@@ -0,0 +1,7 @@
+package com.sethdevkh.ecommerce.system.persistence.business.exception;
+
+public class BusinessPersistenceException extends RuntimeException {
+    public BusinessPersistenceException(String message) {
+        super(message);
+    }
+}
